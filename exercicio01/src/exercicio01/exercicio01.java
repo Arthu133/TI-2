8bbb35290bce6e19f36fefa2eee773cccf8943e6
Arthu133 <84146751+Arthu133@users.noreply.github.com>
2021-09-05 19:44:59 -0300

    Add files via upload

---
+++ exercicio01/src/exercicio01/exercicio01.java
@@ -0,0 +1,32 @@
+
+package exercicio01;
+import java.util.Scanner;
+
+public class exercicio01 {
+	
+
+	
+		public static Scanner sc = new Scanner(System.in);
+
+	    public static void main (String args[]) {
+	 
+	        int n, n2,somatorio;
+
+	        System.out.println("Numero um:");
+	            n = sc.nextInt();
+
+	        System.out.println("Numero dois:");
+	        n2 = sc.nextInt();
+
+
+	        somatorio = n + n2;
+
+	        System.out.println("Sum:"+somatorio);
+
+
+
+	    
+	}
+
+
+}
---
+++ exercicio01/src/exercicio01/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author arthu
+ *
+ */
+package exercicio01;
